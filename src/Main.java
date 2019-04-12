703a7ec984efad3fc5acfd477844f97bb00c10a7
Fernando Pinedo <fernando.pinedo@outlook.com>
2019-04-12 10:10:10 -0500

    Agrega ícono a aplicación

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,10 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+
+import javax.swing.*;
 
 public class Main extends Application {
 
@@ -16,6 +19,16 @@ public class Main extends Application {
     URL fxml = getClass().getClassLoader().getResource("resources/views/Students.fxml");
     FXMLLoader fxmlLoader = new FXMLLoader(fxml);
     Parent parent = fxmlLoader.load();
+    primaryStage.getIcons().add(
+            new Image(getClass().getResource
+                    ("resources/images/keyhole.png").toExternalForm()));
+
+    // mac
+    URL iconURL = Main.class.getResource("resources/images/keyhole.png");
+    java.awt.Image image = new ImageIcon(iconURL).getImage();
+    com.apple.eawt.Application.getApplication().setDockIconImage(image);
+    primaryStage.setTitle("Registro de estudiantes");
+
     primaryStage.setScene(new Scene(parent));
     primaryStage.show();
 
